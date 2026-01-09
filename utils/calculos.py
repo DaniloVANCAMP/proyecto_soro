019448649f8ceb52550402b75ff80e9907a6db8e
DaniloVANCAMP <danilovanoy2@gmail.com>
2026-01-09 15:19:45 -0500

    Update calculos.py

--- utils/calculos.py
+++ utils/calculos.py
@@ -118,8 +118,8 @@ def procesar_datos(params, bitacora):
 
     comparativa = pd.DataFrame({
         "Indicador": ["Equipo / Cuadrilla", "Tiempo Restante", "Utilidad Proyectada", "IMPACTO ($)"],
-        "Tendencia": [f"{int(u['num_ayudantes'])}Ayudantes/{int(u['num_maestros'])}Maestros/{int(r_ult)}Retro", f"{int(dias_t)} días", f_cop(util_t), "---"],
-        "Óptimo": [f"{int(opt['Ayud'])}Ayudantes/{int(opt['Mae'])}Maestros/{int(opt['Retro'])}Retro", f"{int(opt['Días'])} días", f_cop(opt['Utilidad']), f"+ {f_cop(impacto)}"]
+        "Tendencia": [f"{int(u['num_ayudantes'])} Ayudantes/{int(u['num_maestros'])} Maestros/{int(r_ult)} Retro", f"{int(dias_t)} días", f_cop(util_t), "---"],
+        "Óptimo": [f"{int(opt['Ayud'])}Ayudantes/{int(opt['Mae'])} Maestros/{int(opt['Retro'])} Retro", f"{int(opt['Días'])} días", f_cop(opt['Utilidad']), f"+ {f_cop(impacto)}"]
     })
 
     balance = pd.DataFrame({
@@ -136,4 +136,5 @@ def procesar_datos(params, bitacora):
         "viajes": int(opt["Viajes_Dia"])
 
     }
+
 
